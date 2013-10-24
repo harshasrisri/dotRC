e8f995dc75717678cc0a5b09a2e3bbaa012ba08b
Sriharsha Mucheli <harshasrisri@gmail.com>
2013-10-24 01:56:07 -0700

    small errors fixed

--- bash/functions.sh
+++ bash/functions.sh
@@ -6,8 +6,8 @@ function sshnopass () {
 	cat ~/.ssh/id_rsa.pub | ssh $1@$2 'cat >> ~/.ssh/authorized_keys';
 	echo "Enter a name for this connection";
 	read name;
-	echo "alias $name='ssh $1@$2 -o ForwardX11=yes'" >> ~/dotRC/bash/sshnopass;
-	source ~/dotRC/bash/sshnopass;
+	echo "alias $name='ssh $1@$2 -o ForwardX11=yes'" >> $keys_file;
+	source $keys_file;
 }
 
 md() {
