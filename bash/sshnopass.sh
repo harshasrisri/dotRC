cc6adb961a30354aca0eaacd35f88c89613bbb50
Sriharsha Mucheli <harshasrisri@gmail.com>
2013-11-09 02:53:56 -0700

    adding everything afresh

---
+++ bash/sshnopass.sh
@@ -0,0 +1,4 @@
+# startxwin &
+alias ece_server='ssh sriharsha@ece3.ece.arizona.edu -o ForwardX11=yes'
+alias hpc1='ssh sriharsha@login.hpc.arizona.edu -o ForwardX11=yes'
+alias obe21='ssh root@150.135.152.21 -o ForwardX11=yes'
