36e2d9160779a80f8bf20994942c621b1d9c240c
Sriharsha Mucheli <harshasrisri@gmail.com>
2013-11-09 16:59:31 -0700

    bugs in bash files because of rearranging the files

--- bash/sshnopass.sh
+++ bash/sshnopass.sh
@@ -2,3 +2,5 @@
 alias ece_server='ssh sriharsha@ece3.ece.arizona.edu -o ForwardX11=yes'
 alias hpc1='ssh sriharsha@login.hpc.arizona.edu -o ForwardX11=yes'
 alias obe21='ssh root@150.135.152.21 -o ForwardX11=yes'
+alias ece3='ssh sriharsha@ece3.ece.arizona.edu -o ForwardX11=yes'
+alias hpc='ssh sriharsha@login.hpc.arizona.edu -o ForwardX11=yes'
