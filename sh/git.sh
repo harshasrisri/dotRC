d43a1657a4566031e8bd3240b9db38692658cfa5
Sriharsha Mucheli <harshasrisri@gmail.com>
2014-11-08 18:59:31 -0700

    changes enable to use this config in bash/zsh

--- sh/git.sh
+++ sh/git.sh
@@ -1,19 +1,3 @@
-## Utilities
-alias ll='ls --color=auto -lFht'
-alias la='ls --color=auto -a'
-alias lr='ls --color=auto -R'
-alias l='ls --color=auto -CF'
-alias ls='ls --color=auto -CF'
-
-## Favorites
-alias a='./a.out'
-alias ..='cd ..; ls'
-alias .b='vi ~/.bashrc'
-alias ..b='. ~/.bashrc'
-alias .h='vi ~/.bash_history'
-alias .v='vi ~/.vimrc'
-alias x='exit'
-
 ## git related stuff
 alias ga='git add'
 alias gpu='git push'
