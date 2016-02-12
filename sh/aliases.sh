24fdbf2be7b9e57a28bc87ae4d7c8831ee251aa6
Sriharsha Mucheli <harshasrisri@gmail.com>
2016-02-11 17:09:24 -0800

    tweaks to aliases

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -1,23 +1,32 @@
-alias ll='ls --color=auto -lFht'
-alias lla='ls --color=auto -lFhta'
-alias la='ls --color=auto -a'
-alias lr='ls --color=auto -R'
-alias l='ls --color=auto -CF'
 alias ls='ls --color=auto -CF'
-alias l.='ls  --color=auto -d .*'
-alias ..='cd ..; ls'
+alias ll='ls -lFht'
+alias la='ls -a'
+alias lla='ls -lFhta'
+alias lr='ls -R'
+alias l.='ls -d .*'
+alias ll.='ls -lFhtd .*'
+
 alias .v='vi ~/.vimrc'
 alias .m='vi ~/.myshrc'
-alias .b='vi ~/.bashrc'
+
 alias .z='vi ~/.zshrc'
-alias .t='vi ~/.tmux.conf'
-alias ..b='. ~/.bashrc'
 alias ..z='. ~/.zshrc'
+
+alias .b='vi ~/.bashrc'
+alias ..b='. ~/.bashrc'
+
+alias .t='vi ~/.tmux.conf'
 alias ..t='tmux source-file ~/.tmux.conf'
+
+alias ..='cd ..; ls'
+alias .rc='cd $(dirname $(readlink ~/.vimrc))..'
+
 alias x='exit'
+
 alias wget='wget -c'
 alias grep='grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn'
 alias pgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+
 alias scpnoverify="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 alias sshnoverify="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
