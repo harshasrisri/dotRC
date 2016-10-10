617d853e697f6b167f5d2e5a75ed4a656333cde7
Sriharsha Mucheli <srmuchel@cisco.com>
2016-10-10 15:59:06 -0700

    removed utf8 and change mouse settings from tmux.conf

--- sh/exports.sh
+++ sh/exports.sh
@@ -1,6 +1,4 @@
-export PATH=$PATH:~/bin
+# export PATH=$PATH:~/bin
 export CSCOPE_DIR=~/.autotags/byhash
-export EDITOR=/usr/bin/vim
-export BROWSER=/usr/bin/firefox
-export CSCOPE_DIR=~/.autotags
+export EDITOR=vim
 export GREP_OPTIONS=
