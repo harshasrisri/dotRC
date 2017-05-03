0dce05985ac2c7cb473b5975b280e4edc734198f
Sriharsha Mucheli <srmuchel@cisco.com>
2017-05-02 20:02:43 -0700

    cleanup interop b/w bash and zsh. migrate to zim

--- sh/aliases.sh
+++ sh/aliases.sh
@@ -8,20 +8,14 @@ alias ll.='ls -lFhtd .*'
 
 alias .v='vi ~/.vimrc'
 alias .m='vi ~/.myshrc'
-
 alias .z='vi ~/.zshrc'
-alias ..z='. ~/.zshrc'
-
-alias .b='vi ~/.bashrc'
-alias ..b='. ~/.bashrc'
-
 alias .t='vi ~/.tmux.conf'
-alias ..t='tmux source-file ~/.tmux.conf'
-
 alias .rc='cd $(dirname $(readlink ~/.vimrc))/..'
 
-alias x='exit'
+alias ..z='. ~/.zshrc'
+alias ..t='tmux source-file ~/.tmux.conf'
 
+alias x='exit'
 alias wget='wget -c'
 alias grep='grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn'
 alias pgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
@@ -29,18 +23,8 @@ alias pgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
 alias scpnoverify="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 alias sshnoverify="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
-alias ga='git add'
-alias gpu='git push'
-alias gpl='git pull'
-alias gcl='git clone'
 alias gl='git log --oneline --decorate --date=short --graph'
 alias gs='git status'
 alias gd='git diff'
 alias gdt='git difftool'
 alias gdc='git diff --cached'
-alias gcm='git commit -m'
-alias gcam='git commit -am'
-alias gbr='git branch'
-alias gco='git checkout'
-alias gra='git remote add'
-alias grr='git remote rm'
--- sh/exports.sh
+++ sh/exports.sh
@@ -1,4 +1,3 @@
-# export PATH=$PATH:~/bin
 export CSCOPE_DIR=~/.autotags/byhash
 export EDITOR=vim
 export GREP_OPTIONS=
