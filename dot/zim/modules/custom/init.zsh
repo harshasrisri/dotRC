645dd0318893be9e3c238e0fb854269947d2ceff
Sriharsha Mucheli <sriharsham@splunk.com>
2021-09-22 09:21:59 -0700

    Refactor setup, zimrc, custom and t(ab)mux paths

--- dot/zim/modules/custom/init.zsh
+++ dot/zim/modules/custom/init.zsh
@@ -5,4 +5,7 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
 source ~/.myshrc
