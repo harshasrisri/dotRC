d500c053cb1d407e1c6bdcb2e11e9f7c36321036
Sriharsha Mucheli <harshasrisri@gmail.com>
2021-09-26 11:41:08 -0700

    Remove p10k conf. refactor setup. debug myenv

--- dot/zim/modules/custom/init.zsh
+++ dot/zim/modules/custom/init.zsh
@@ -5,9 +5,6 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
 # zstyle rules for fzf-tab completion
 source $(dirname $0)/zstyle.zsh
 
