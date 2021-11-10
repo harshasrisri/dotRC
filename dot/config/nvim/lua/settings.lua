6e66ec5d62749896f411f5a7d07f959c01fd4034
Sriharsha Mucheli <harshasrisri@gmail.com>
2021-11-10 12:23:47 -0800

    Nvim: freeze lsp_signature. intro lsp_renamer

--- dot/config/nvim/lua/settings.lua
+++ dot/config/nvim/lua/settings.lua
@@ -2,7 +2,6 @@ local disabled_built_ins = {
   "netrw", "netrwPlugin", "netrwSettings", "netrwFileHandlers",
   "gzip", "zip", "zipPlugin", "tar", "tarPlugin",
   "getscript", "getscriptPlugin",
-  "vimball", "vimballPlugin",
   "2html_plugin", "logipat", "rrhelper", "spellfile_plugin", "matchit",
 }
 
