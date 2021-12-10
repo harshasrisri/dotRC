d54105dd5fe72ca4bed0594d19544a7d60444085
Sriharsha Mucheli <harshasrisri@gmail.com>
2021-12-10 15:28:27 -0800

    add c and lua to lsp

--- dot/config/nvim/init.lua
+++ dot/config/nvim/init.lua
@@ -1,6 +1,6 @@
 local present, impatient = pcall(require, "impatient")
 if not present then
-    error("impatient not loaded yet")
+    print("impatient not loaded yet")
 end
 require('settings')
 require('mappings')
