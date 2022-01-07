416f56622cd8e8fb5ec87e6eea966ec5978de432
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-01-07 10:47:15 -0800

    tweaks to lsp signs and material

--- dot/config/nvim/lua/plugins/lsp.lua
+++ dot/config/nvim/lua/plugins/lsp.lua
@@ -35,3 +35,9 @@ for _, lsp in ipairs(servers) do
         }
     }
 end
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
