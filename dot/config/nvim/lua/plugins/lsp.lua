84ed911923161e8ddeb6b937278c6fcedb43e965
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-11-02 16:42:42 -0700

    overhaul mason/lspconfig and make it consistent

--- dot/config/nvim/lua/plugins/lsp.lua
+++ dot/config/nvim/lua/plugins/lsp.lua
@@ -1,27 +1,44 @@
 local nvim_lsp = require('lspconfig')
 
--- list of langservers to configure
-local servers = { "ccls", "rust_analyzer", "gopls", "jdtls", "pylsp", "sumneko_lua" }
-
 -- populate completion engine with language specific LSP capabilities
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
--- Use a loop to conveniently call 'setup' on multiple servers and map buffer local keybindings when the language server attaches
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-        capabilities = capabilities,
-        flags = {
-            debounce_text_changes = 150,
-        }
-    }
-end
-
+-- ccls not managed by Mason. configuring it manually
 nvim_lsp.ccls.setup {
+    capabilities = capabilities,
+    flags = {
+        debounce_text_changes = 150,
+    },
   init_options = {
     index = {
       threads = 16;
     };
   }
+}
+
+require('mason-lspconfig').setup_handlers {
+    function (server_name)
+        nvim_lsp[server_name].setup {
+            capabilities = capabilities,
+            flags = {
+                debounce_text_changes = 150,
+            }
+        }
+    end,
+    ["rust_analyzer"] = function ()
+        require("rust-tools").setup {}
+    end,
+    ["sumneko_lua"] = function ()
+        nvim_lsp.sumneko_lua.setup {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" }
+                    }
+                }
+            }
+        }
+    end,
 }
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
