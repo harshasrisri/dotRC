8a330f2f0af24f3b833c94ab85a9c7a82b1e54e8
Sriharsha Mucheli <harshasrisri@gmail.com>
2023-01-30 15:52:17 -0800

    Move Gitsigns mappings to plugin spec

--- dot/config/nvim/lua/mappings.lua
+++ dot/config/nvim/lua/mappings.lua
@@ -90,24 +90,3 @@ map('n', '<leader>lx', '<cmd>Telescope lsp_references<CR>')
 map('n', '<leader>et', '<cmd>NvimTreeToggle<CR>')
 map('n', '<leader>ef', '<cmd>NvimTreeFocus<CR>')
 map('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>')
-
--- Git Signs
-    -- Navigation
-    map('n', ']c', '<cmd>Gitsigns next_hunk<CR>')
-    map('n', '[c', '<cmd>Gitsigns prev_hunk<CR>')
-
-    -- Actions
-    map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-    map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
-    map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
-    map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
-    map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
-    map('n', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
-    map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
-    map('v', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
-    map('v', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
-
-    -- Text object
-    map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-    map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
