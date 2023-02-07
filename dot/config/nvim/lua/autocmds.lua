5282d64d91778ae91cf48be0be1356dc2b44c67f
Sriharsha Mucheli <harshasrisri@gmail.com>
2023-02-06 21:06:31 -0800

    Fix indents

--- dot/config/nvim/lua/autocmds.lua
+++ dot/config/nvim/lua/autocmds.lua
@@ -24,14 +24,14 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, { comma
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-  callback = function()
-    vim.cmd("tabdo wincmd =")
-  end,
+    callback = function()
+        vim.cmd("tabdo wincmd =")
+    end,
 })
