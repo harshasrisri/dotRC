ffa1c49a12628ee5d991c78966a88164930b4af1
Sriharsha Mucheli <harshasrisri@gmail.com>
2023-02-07 09:11:55 -0800

    Refactor to lazy load lspconfig

--- dot/config/nvim/lua/plugins/code.lua
+++ dot/config/nvim/lua/plugins/code.lua
@@ -54,13 +54,4 @@ return {
         end,
         config = true,
     },
-
-    {
-        'hashivim/vim-terraform',
-        ft = { 'terraform', 'hcl', 'terraform-vars' },
-        config = function()
-            vim.g.terraform_fmt_on_save = 1
-            vim.g.terraform_align = 1
-        end
-    },
 }
