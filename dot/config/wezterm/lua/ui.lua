990c8fc123f2cc95e56dc576ec312e7747229801
Sriharsha Mucheli <sriharsham@splunk.com>
2024-08-26 00:04:29 -0700

    Setup tab bar segments and colors

--- dot/config/wezterm/lua/ui.lua
+++ dot/config/wezterm/lua/ui.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+
 local function init(config)
     local color_scheme, _ = wezterm.color.load_scheme(wezterm.home_dir .. "/.config/wezterm/oxide.toml")
     config.color_schemes = {
@@ -7,21 +8,63 @@ local function init(config)
     config.color_scheme = 'Oxide'
 
     config.font_size = 16
-    config.font = wezterm.font {
-        family = 'IosevkaTerm Nerd Font Propo',
-        stretch = 'ExtraCondensed',
-    }
+    config.font = wezterm.font 'IosevkaTerm Nerd Font Propo'
 
-    config.tab_bar_at_bottom = true
     config.window_padding = { left = 1, right = 1, top = 1, bottom = 1 }
     config.bold_brightens_ansi_colors = true
     config.window_decorations = "RESIZE"
 
-    wezterm.on('update-right-status', function(window, _)
-        local name = window:active_key_table()
-        if name then
-            name = 'TABLE: ' .. name
-        end
+    config.use_fancy_tab_bar = false
+    config.tab_max_width = 32
+
+    wezterm.on("update-status", function(window, _)
+        local status_generator = require("wez-status-generator.plugin")
+        local status = status_generator.generate_left_status({
+            sections = {
+                {
+                    components = { function() return wezterm.hostname() end, },
+                    foreground = "black",
+                    background = "indianred",
+                },
+                {
+                    components = { function() return window:mux_window():get_workspace() end, },
+                    foreground = "black",
+                    background = "orange",
+                },
+            },
+            separator = status_generator.section_separators.SLANT_REVERSE,
+            hide_empty_sections = true,
+        })
+
+        window:set_left_status(status)
+    end)
+
+    wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+        local tab_id = tonumber(tab.tab_index) + 1
+        local procname = require('util').basename(tab.active_pane.foreground_process_name)
+        return string.format(' %s(%s): %s ', tab_id, #panes, procname)
+    end)
+
+    wezterm.on("update-status", function(window, _)
+        local status_generator = require("wez-status-generator.plugin")
+        local status = status_generator.generate_right_status({
+            sections = {
+                {
+                    components = { function() return wezterm.strftime("%H:%M:%S") end, },
+                    foreground = "black",
+                    background = "orange",
+                },
+                {
+                    components = { function() return wezterm.strftime("%d-%b-%y") end, },
+                    foreground = "black",
+                    background = "indianred",
+                },
+            },
+            separator = status_generator.section_separators.SLANT_REVERSE,
+            hide_empty_sections = true,
+        })
+
+        window:set_right_status(status)
     end)
 end
 
--- dot/config/wezterm/lua/util.lua
+++ dot/config/wezterm/lua/util.lua
@@ -1,4 +1,14 @@
-function FileExists(name)
+local M = {}
+function M.file_exists(name)
    local f=io.open(name,"r")
    if f~=nil then io.close(f) return true else return false end
 end
+
+-- Equivalent to POSIX basename(3)
+-- Given "/foo/bar" returns "bar"
+-- Given "c:\\foo\\bar" returns "bar"
+function M.basename(s)
+  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
+return M
