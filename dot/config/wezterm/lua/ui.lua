2b09ec63f01408a8da6c3e1241e987b55216e060
Sriharsha Mucheli <sriharsham@splunk.com>
2024-08-26 17:26:15 -0700

    Remove wez-status-generator plugin

--- dot/config/wezterm/lua/ui.lua
+++ dot/config/wezterm/lua/ui.lua
@@ -1,4 +1,6 @@
 local wezterm = require("wezterm")
+local SOLID_BOT_LEFT_SLANT = ""
+local SOLID_BOT_RIGHT_SLANT = ''
 
 local function init(config)
     local color_scheme, _ = wezterm.color.load_scheme(wezterm.home_dir .. "/.config/wezterm/oxide.toml")
@@ -14,57 +16,49 @@ local function init(config)
     config.bold_brightens_ansi_colors = true
     config.window_decorations = "RESIZE"
 
+    config.tab_bar_at_bottom = true
     config.use_fancy_tab_bar = false
     config.tab_max_width = 32
 
-    wezterm.on("update-status", function(window, _)
-        local status_generator = require("wez-status-generator.plugin")
-        local status = status_generator.generate_left_status({
-            sections = {
-                {
-                    components = { function() return wezterm.hostname() end, },
-                    foreground = "black",
-                    background = "indianred",
-                },
-                {
-                    components = { function() return window:mux_window():get_workspace() end, },
-                    foreground = "black",
-                    background = "orange",
-                },
-            },
-            separator = status_generator.section_separators.SLANT_REVERSE,
-            hide_empty_sections = true,
-        })
-
-        window:set_left_status(status)
-    end)
-
-    wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+    wezterm.on('format-tab-title', function(tab, _, _, _, _, _)
         local tab_id = tonumber(tab.tab_index) + 1
         local procname = require('util').basename(tab.active_pane.foreground_process_name)
-        return string.format(' %s(%s): %s ', tab_id, #panes, procname)
+        return string.format('  %s: %s  ', tab_id, procname)
     end)
 
     wezterm.on("update-status", function(window, _)
-        local status_generator = require("wez-status-generator.plugin")
-        local status = status_generator.generate_right_status({
-            sections = {
-                {
-                    components = { function() return wezterm.strftime("%H:%M:%S") end, },
-                    foreground = "black",
-                    background = "orange",
-                },
-                {
-                    components = { function() return wezterm.strftime("%d-%b-%y") end, },
-                    foreground = "black",
-                    background = "indianred",
-                },
-            },
-            separator = status_generator.section_separators.SLANT_REVERSE,
-            hide_empty_sections = true,
-        })
+        local segments = {
+            window:active_workspace(),
+            wezterm.strftime('%a %b %-d %H:%M:%S'),
+            wezterm.hostname(),
+        }
+        local colors = { 'gold', 'orange', 'indianred' }
 
-        window:set_right_status(status)
+        local right_elements = {}
+
+        for i, seg in ipairs(segments) do
+            local is_first = i == 1
+            if is_first then
+                table.insert(right_elements, { Background = { Color = 'none'}})
+            end
+            table.insert(right_elements, { Foreground = { Color = colors[i] }})
+            table.insert(right_elements, { Text = SOLID_BOT_RIGHT_SLANT})
+            table.insert(right_elements, { Foreground = { Color = 'black' }})
+            table.insert(right_elements, { Background = { Color = colors[i] }})
+            table.insert(right_elements, { Text = string.format(' %s ', seg)})
+        end
+
+        local left_element = {
+            { Foreground = { Color = 'black' }},
+            { Background = { Color = 'indianred' }},
+            { Text = '  WezTerm ' },
+            { Foreground = { Color = 'indianred'}},
+            { Background = { Color = 'none' }},
+            { Text = SOLID_BOT_LEFT_SLANT }
+        }
+
+        window:set_left_status(wezterm.format(left_element))
+        window:set_right_status(wezterm.format(right_elements))
     end)
 end
 
