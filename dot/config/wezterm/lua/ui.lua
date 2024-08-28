406fd97cdfbdd2dc7c7b2ad0eb002a3bb2215cb5
Sriharsha Mucheli <harshasrisri@gmail.com>
2024-08-28 11:27:26 -0700

    format_element function to parametrize styling tab elements

--- dot/config/wezterm/lua/ui.lua
+++ dot/config/wezterm/lua/ui.lua
@@ -1,6 +1,35 @@
 local wezterm = require("wezterm")
-local SOLID_BOT_LEFT_SLANT = ""
-local SOLID_BOT_RIGHT_SLANT = ''
+local Left = 'left'
+local Right = 'right'
+local Direction = { Left, Right }
+
+local function format_element(direction, bg, fg, text, intensity, italic)
+    local SOLID_LEFT_SLANT_START = ""
+    local SOLID_LEFT_SLANT_END = ""
+
+    local SOLID_RIGHT_SLANT_START = ''
+    local SOLID_RIGHT_SLANT_END = ''
+
+    local tab_bar_bg = '#222222'
+
+    local start_edge, end_edge
+    if direction == Direction.Left then
+        start_edge = SOLID_LEFT_SLANT_START
+        end_edge = SOLID_LEFT_SLANT_END
+    else
+        start_edge = SOLID_RIGHT_SLANT_START
+        end_edge = SOLID_RIGHT_SLANT_END
+    end
+
+    return {
+        { Background = { Color = tab_bar_bg } }, { Foreground = { Color = bg } }, { Attribute = { Intensity = intensity } }, { Attribute = { Italic = italic } },
+        { Text = start_edge },
+        { Background = { Color = bg } }, { Foreground = { Color = fg } }, { Attribute = { Intensity = intensity } }, { Attribute = { Italic = italic } },
+        { Text = text },
+        { Background = { Color = tab_bar_bg } }, { Foreground = { Color = bg } }, { Attribute = { Intensity = intensity } }, { Attribute = { Italic = italic } },
+        { Text = end_edge },
+    }
+end
 
 local function set_status()
     wezterm.on("update-status", function(window, _)
@@ -14,62 +43,52 @@ local function set_status()
         local right_elements = {}
 
         for i, seg in ipairs(segments) do
-            table.insert(right_elements, { Background = { Color = 'none'}})
-            table.insert(right_elements, { Foreground = { Color = colors[i] }})
-            table.insert(right_elements, { Text = SOLID_BOT_RIGHT_SLANT})
-            table.insert(right_elements, { Foreground = { Color = 'black' }})
-            table.insert(right_elements, { Background = { Color = colors[i] }})
-            table.insert(right_elements, { Text = string.format(' %s ', seg)})
-            if i < #segments then
-                table.insert(right_elements, { Foreground = { Color = 'none'}})
-                table.insert(right_elements, { Text = SOLID_BOT_RIGHT_SLANT})
+            local right_seg = format_element(Direction.Right, colors[i], 'black', seg, 'Normal', false)
+            for item in right_seg do
+                table.insert(right_elements, item)
             end
         end
 
-        local left_element = {
-            { Foreground = { Color = 'black' }},
-            { Background = { Color = 'indianred' }},
-            { Text = '  ' .. wezterm.nerdfonts.dev_terminal .. ' WezTerm ' },
-            { Foreground = { Color = 'indianred'}},
-            { Background = { Color = 'none' }},
-            { Text = SOLID_BOT_LEFT_SLANT },
-            { Foreground = { Color = 'none' }},
-            { Text = '  '},
-        }
+        local left_seg = '  ' .. wezterm.nerdfonts.dev_terminal .. ' WezTerm '
+        local left_elemen = format_element(Direction.Left, 'indianred', 'black', left_seg, 'Normal', false)
 
-        window:set_left_status(wezterm.format(left_element))
+        window:set_left_status(wezterm.format(left_elemen))
         window:set_right_status(wezterm.format(right_elements))
+
     end)
 end
 
-
 local function format_tab_bar(config)
-    config.colors              = {
-        tab_bar                = {
-            background         = '#222222',
-            new_tab            = { bg_color = '#444444', fg_color = 'black', },
-            new_tab_hover      = { bg_color = 'lightcoral', fg_color = 'black', },
+    config.colors = {
+        tab_bar = {
+            background = '#222222',
+            new_tab = { bg_color = '#444444', fg_color = 'black', },
+            new_tab_hover = { bg_color = 'lightcoral', fg_color = 'black', },
         },
+    }
+
+    config.tab_bar_style = {
+        new_tab = wezterm.format( format_element(Direction.Left, '#444444', 'black', ' + ', 'Normal', false) ),
+        new_tab_hover = wezterm.format( format_element(Direction.Left, '#CCCCCC', 'black', ' + ', 'Bold', false) ),
     }
 
     wezterm.on('format-tab-title', function(tab, _, _, _, hover, max_width)
         local tab_id = tonumber(tab.tab_index) + 1
         local procname = require('util').basename(tab.active_pane.foreground_process_name)
-        local tab_bar_bg = '#222222'
 
-        local tab_fg = 'black'
-        local tab_bg = 'coral'
-        local intensity = 'Half'
+        local tab_bg = 'black'
+        local tab_fg = 'coral'
+        local intensity = 'Normal'
         local italic = true
 
         if tab.is_active then
-            tab_fg = 'coral'
-            tab_bg = 'black'
+            tab_bg = 'coral'
+            tab_fg = 'black'
             intensity = 'Bold'
             italic = false
         elseif hover then
-            tab_bg = '#444444'
-            tab_fg = 'coral'
+            tab_fg = '#444444'
+            tab_bg = 'lightgoldenrod'
             intensity = 'Normal'
             italic = false
         end
@@ -77,14 +96,7 @@ local function format_tab_bar(config)
         local title = string.format(' %s: %s ', tab_id, procname)
         title = wezterm.truncate_right(title, max_width - 2)
 
-        return {
-            { Background = { Color = tab_fg } }, { Foreground = { Color = tab_bar_bg } }, { Attribute = { Intensity = intensity } }, { Attribute = { Italic = italic } },
-            { Text = SOLID_BOT_LEFT_SLANT },
-            { Background = { Color = tab_fg } }, { Foreground = { Color = tab_bg } }, { Attribute = { Intensity = intensity } }, { Attribute = { Italic = italic } },
-            { Text = title },
-            { Background = { Color = tab_bar_bg } }, { Foreground = { Color = tab_fg } }, { Attribute = { Intensity = intensity } }, { Attribute = { Italic = italic } },
-            { Text = SOLID_BOT_LEFT_SLANT },
-        }
+        return format_element(Direction.Left, tab_bg, tab_fg, title, intensity, italic)
     end)
 end
 
