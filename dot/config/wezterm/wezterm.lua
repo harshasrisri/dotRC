c77ccd54f8c025a126cbe23d6148610f9336ad48
Sriharsha Mucheli <harshasrisri@gmail.com>
2024-09-17 23:35:08 -0700

    Wezterm conf tweaks

--- dot/config/wezterm/wezterm.lua
+++ dot/config/wezterm/wezterm.lua
@@ -5,6 +5,11 @@ package.path = package.path .. ";" .. wezterm.config_dir .. "/lua/?.lua"
 local config = wezterm.config_builder()
 
 config.status_update_interval = 100
+config.scrollback_lines = 100000
+
+config.unix_domains = { { name = 'local_mux', }, }
+config.default_mux_server_domain = 'local_mux'
+config.term = 'wezterm'
 
 require("ui")(config)
 require("keymap")(config)
@@ -13,7 +18,5 @@ local status, local_config = pcall(require, "local")
 if status then
     local_config.init(config)
 end
-
-config.unix_domains = { { name = 'unix', }, }
 
 return config
