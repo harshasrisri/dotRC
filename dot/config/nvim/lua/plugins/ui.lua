f755eac04ce66db09c0f260aa8c352093bd0b9d5
Sriharsha Mucheli <harshasrisri@gmail.com>
2024-11-20 21:54:18 -0800

    start barbecue without lazy to prevent stutter

--- dot/config/nvim/lua/plugins/ui.lua
+++ dot/config/nvim/lua/plugins/ui.lua
@@ -43,7 +43,6 @@ return {
 
     {
         'utilyre/barbecue.nvim',
-        event = "VeryLazy",
         dependencies = {
             "SmiteshP/nvim-navic",
             "nvim-tree/nvim-web-devicons",
