2e7e10aed534e9aad5ecc4084382abd718ac0c45
Sriharsha Mucheli <harshasrisri@gmail.com>
2025-03-15 08:59:00 -0700

    Add unicode section separator in lualine

--- dot/config/nvim/lua/plugins/ui.lua
+++ dot/config/nvim/lua/plugins/ui.lua
@@ -19,7 +19,7 @@ return {
             options = {
                 icons_enabled = true,
                 theme = 'ayu_dark',
-                component_separators = { left = '\\', right = '/'},
+                component_separators = { left = '', right = ''},
                 section_separators = { left = '', right = ''},
                 disabled_filetypes = {
                     statusline = {},
