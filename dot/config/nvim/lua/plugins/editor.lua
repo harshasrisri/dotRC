064f95176cfdaa55b157853e2091ee99763afc45
Sriharsha Mucheli <harshasrisri@gmail.com>
2025-04-28 22:57:24 -0700

    Replace Telescope and few other plugins with Snacks.nvim

--- dot/config/nvim/lua/plugins/editor.lua
+++ dot/config/nvim/lua/plugins/editor.lua
@@ -17,12 +17,12 @@ return {
         }
     },
 
-    {
-        'ahmedkhalf/project.nvim',
-        event = 'VeryLazy',
-        -- Telescope extension and mapping loaded in Telescope config
-        config = function() require('project_nvim').setup() end
-    },
+    -- {
+    --     'ahmedkhalf/project.nvim',
+    --     event = 'VeryLazy',
+    --     -- Telescope extension and mapping loaded in Telescope config
+    --     config = function() require('project_nvim').setup() end
+    -- },
 
     {
         'AckslD/nvim-neoclip.lua',
@@ -68,51 +68,5 @@ return {
             vim.api.nvim_set_hl(0, 'LeapBackDrop', { link = 'Comment' })
             require('leap').add_default_mappings()
         end
-    },
-
-    {
-        'nvim-tree/nvim-tree.lua',
-        cmd = 'NvimTreeToggle',
-        keys = {
-            { '<leader>et', '<cmd>NvimTreeToggle<CR>', desc = "Toggle NvimTree" }
-        },
-        dependencies = 'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        opts = {
-            sync_root_with_cwd = true,
-            respect_buf_cwd = true,
-            update_focused_file = {
-                enable = true,
-                update_root = true
-            },
-            view = {
-                width = 36,
-            },
-            renderer = {
-                highlight_git = true,
-                highlight_opened_files = "all",
-                indent_markers = {
-                    enable = true,
-                },
-                icons = {
-                    git_placement = 'signcolumn',
-                }
-            },
-            actions = {
-                open_file = {
-                    window_picker = {
-                        enable = false,
-                    }
-                }
-            }
-        },
-    },
-
-    {
-        'echasnovski/mini.map',
-        lazy = true,
-        keys = {
-            { "<leader>mm", "<cmd>lua require('mini.map').toggle()<CR>", desc = "Open Mini Map" },
-        },
-        opts = {},
     },
 }
--- dot/config/nvim/lua/plugins/util.lua
+++ dot/config/nvim/lua/plugins/util.lua
@@ -1,10 +1,6 @@
 return {
     { 'tpope/vim-obsession' },
-
-    {
-        'tpope/vim-fugitive',
-        cmd = { "Git", "Gdiff", "Gdiffsplit", "Gvdiffsplit", "Gwrite", "Gw" }
-    },
+    { 'nvim-lua/plenary.nvim', lazy = true },
 
     {
         'lewis6991/gitsigns.nvim',
@@ -14,14 +10,12 @@ return {
             { ']h', '<cmd>Gitsigns next_hunk<CR>' },
             { '[h', '<cmd>Gitsigns prev_hunk<CR>' },
             -- Actions
-            { '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>' },
-            { '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>' },
-            { '<leader>hd', '<cmd>Gitsigns diffthis<CR>' },
-            { '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>' },
-            { '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>' },
-            { '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>' },
-            { '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>' },
-            { '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>' },
+            { '<leader>gR', '<cmd>Gitsigns reset_buffer<CR>',    desc = "Git reset buffer" },
+            { '<leader>gS', '<cmd>Gitsigns stage_buffer<CR>',    desc = "Git stage buffer" },
+            { '<leader>gp', '<cmd>Gitsigns preview_hunk<CR>',    desc = "Git preview hunk" },
+            { '<leader>gr', '<cmd>Gitsigns reset_hunk<CR>',      desc = "Git reset hunk" },
+            { '<leader>gs', '<cmd>Gitsigns stage_hunk<CR>',      desc = "Git stage hunk" },
+            { '<leader>gU', '<cmd>Gitsigns undo_stage_hunk<CR>', desc = "Git undo stage hunk" },
             -- Text object
             { 'ih', '<cmd><C-U>Gitsigns select_hunk<CR>', mode = 'o' },
             { 'ih', '<cmd><C-U>Gitsigns select_hunk<CR>', mode = 'x'},
@@ -111,11 +105,105 @@ return {
                 github = {
                     url = "github.com",
                     blob = "/blob/",
+                    blame = "/blame/",
                     line_format = "#L%s-L%s",
                 },
             }
         },
     },
 
-    { 'nvim-lua/plenary.nvim', lazy = true },
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        keys = {
+            { "<leader>bd",  function() require('snacks').bufdelete.delete() end,             desc = "Delete Buffer" },
+            { "<leader>sb",  function() require('snacks').scratch() end,                      desc = "Scratch Buffer" },
+            { "<leader>fe",  function() require('snacks').explorer() end,                     desc = "File Explorer" },
+            -- Git stuff
+            { "<leader>gb",  function() require('snacks').gitbrowse() end,                    desc = "Open line(s) in git repo", mode = { 'n', 'x' } },
+            { "<leader>gl",  function() require('snacks').picker.git_log() end,               desc = "Git Log" },
+            { "<leader>glf", function() require('snacks').picker.git_log_file() end,          desc = "Git Log File" },
+            { "<leader>gll", function() require('snacks').picker.git_log_line() end,          desc = "Git Log Line" },
+            { "<leader>gui", function() require('snacks').terminal.toggle("gitui") end,       desc = "Git UI in float window"},
+            -- LSP pickers
+            { "<leader>lD",  function() require('snacks').picker.lsp_declarations() end,      desc = "Goto Declaration" },
+            { "<leader>ld",  function() require('snacks').picker.lsp_definitions() end,       desc = "Goto Definition" },
+            { "<leader>li",  function() require('snacks').picker.lsp_implementations() end,   desc = "Goto Implementation" },
+            { "<leader>lx",  function() require('snacks').picker.lsp_references() end,        desc = "References", nowait = true },
+            { "<leader>ls",  function() require('snacks').picker.lsp_symbols() end,           desc = "LSP Symbols" },
+            { "<leader>lS",  function() require('snacks').picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+            { "<leader>lt",  function() require('snacks').picker.lsp_type_definitions() end,  desc = "Goto Type Definition" },
+            -- Fuzzy pickers
+            { "<leader>f:",  function() require('snacks').picker.command_history() end,       desc = "Command History" },
+            { '<leader>f?',  function() require('snacks').picker.search_history() end,        desc = "Search Search History" },
+            { "<leader>fa",  function() require('snacks').picker.autocmds() end,              desc = "Search Autocmds" },
+            { "<leader>fb",  function() require('snacks').picker.buffers() end,               desc = "Buffers" },
+            { "<leader>fc",  function() require('snacks').picker.commands() end,              desc = "Commands" },
+            { "<leader>ff",  function() require('snacks').picker.files() end,                 desc = "Find Files" },
+            { "<leader>fh",  function() require('snacks').picker.help() end,                  desc = "Search Help Pages" },
+            { "<leader>fi",  function() require('snacks').picker.icons() end,                 desc = "Search Icons" },
+            { "<leader>fj",  function() require('snacks').picker.jumps() end,                 desc = "Search Jumps" },
+            { "<leader>fk",  function() require('snacks').picker.keymaps() end,               desc = "Search Keymaps" },
+            { "<leader>fm",  function() require('snacks').picker.man() end,                   desc = "Search Man Pages" },
+            { "<leader>fn",  function() require('snacks').picker.notifications() end,         desc = "Notification History" },
+            { "<leader>fp",  function() require('snacks').picker.projects() end,              desc = "Projects" },
+            { "<leader>fr",  function() require('snacks').picker.resume() end,                desc = "Resume" },
+            { "<leader>fR",  function() require('snacks').picker.recent() end,                desc = "Recent History" },
+            { "<leader>fs",  function() require('snacks').picker() end,                       desc = "Snacks Picker" },
+            { "<leader>fS",  function() require('snacks').scratch.select() end,               desc = "Scratch Buffer Picker" },
+            { "<leader>fu",  function() require('snacks').picker.undo() end,                  desc = "Search Undo History" },
+            { "<leader>fy",  function() require('snacks').picker.cliphist() end,              desc = "Search Clipboard History" },
+            -- Grep pickers
+            { "<leader>/l",  function() require('snacks').picker.lines() end,                 desc = "Grep Buffer Lines" },
+            { "<leader>/b",  function() require('snacks').picker.grep_buffers() end,          desc = "Grep Open Buffers" },
+            { "<leader>/w",  function() require('snacks').picker.grep_word() end,             desc = "Grep selection or word", mode = { "n", "x" } },
+            { "<leader>/L",  function() require('snacks').picker.grep() end,                  desc = "Live Grep PWD" },
+            {
+                "<leader>//",
+                function()
+                    require('snacks').input({ win = { row = 0.33 }, prompt = "Search for > " }, function(input) 
+                        if (input == "") then return end
+                        require('snacks').picker.grep({ regex = false, search = input, live = false })
+                    end)
+                end,
+                desc = "Search with ripgrep",
+            },
+        },
+        opts = {
+            animate = { duration = 10 },
+            bigfile = { enabled = true },
+            bufdelete = { enabled = true },
+            dashboard = {
+                sections = {
+                    { section = "header" },
+                    { title = "Recent Files (PWD) ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
+                    { section = "recent_files", cwd = true, limit = 8, padding = 1 },
+                    { title = "Recent Files (System)", padding = 1 },
+                    { section = "recent_files", limit = 8, padding = 1 },
+                    { title = "Projects", padding = 1 },
+                    { section = "projects", padding = 1 },
+                    { title = "Bookmarks", padding = 1 },
+                    { section = "keys", padding = 1 },
+                    { section = "startup" },
+                },
+            },
+            explorer = { replace_netrw = true },
+            gitbrowse = { what = "permalink" },
+            indent = { enabled = true },
+            picker = { layout = {
+                cycle = true,
+                preset = function ()
+                    return vim.o.columns >= 120 and 'telescope' or 'vertical'
+                end
+            }},
+            quickfile = { enabled = true },
+            scratch = { enabled = true },
+            scope = { enabled = true },
+            scroll = { enabled = true },
+            statuscolumn = { enabled = true },
+            toggle = { enabled = true },
+            words = { enabled = true },
+        },
+    },
 }
