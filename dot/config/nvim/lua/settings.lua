25b2c1b29876b397cd01ef641fb62ca589772544
Sriharsha Mucheli <harshasrisri@gmail.com>
2025-05-06 08:44:55 -0700

    Set borders for all floating windows

--- dot/config/nvim/lua/settings.lua
+++ dot/config/nvim/lua/settings.lua
@@ -60,8 +60,8 @@ end
 vim.cmd[[ hi VertSplit ctermbg=NONE guibg=NONE]]
 
 -- Set borders for all floating windows globally
--- vim.cmd [[autocmd ColorScheme * highlight NormalFloat guibg=#1f2335]]
--- vim.cmd [[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+vim.cmd [[autocmd ColorScheme * highlight NormalFloat guibg=#1f2335]]
+vim.cmd [[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
 local border = {
     {"╭", "FloatBorder"},
     {"─", "FloatBorder"},
