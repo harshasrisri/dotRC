b02978ee9c547ebfd05d1d09aaf113a6486be3ff
Sriharsha Mucheli <harshasrisri@gmail.com>
2025-11-13 01:27:58 -0800

    Optimize fzf-tab and its previewer

--- dot/config/zim/custom/init.zsh
+++ dot/config/zim/custom/init.zsh
@@ -9,3 +9,16 @@
 source $(dirname $0)/zstyle.zsh
 
 source ~/.myshrc
+
+# Enable completions for wrapper functions
+compdef gri=git      # git rebase completions for gri
+compdef frg=rg       # ripgrep completions for frg
+compdef frgv=rg      # ripgrep completions for frgv
+
+# Custom completion for tm (tmux sessions)
+_tmux-sessions() {
+    local sessions
+    sessions=(${(f)"$(tmux list-sessions -F '#S' 2>/dev/null)"})
+    _describe 'tmux sessions' sessions
+}
+compdef _tmux-sessions tm
--- dot/config/zim/custom/zstyle.zsh
+++ dot/config/zim/custom/zstyle.zsh
@@ -20,12 +20,15 @@ zstyle ':fzf-tab:*' switch-group '<' '>'
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
 zstyle ':fzf-tab:*' popup-pad 240 160
 
+# When not in tmux, show more lines (40 instead of default ~10)
+zstyle ':fzf-tab:*' fzf-min-height 40
+
 # Use eza to preview directory during cd
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 
 # give a preview of commandline arguments when completing `kill`
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
-zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview '[[ $group == "[process ID]" ]] && ps --pid=$word -o cmd --no-headers -w -w'
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview '[[ $group == "[process ID]" ]] && ps -p $word -o command'
 zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags --preview-window=down:3:wrap
 
 # Show systemd unit status
@@ -38,7 +41,7 @@ zstyle ':fzf-tab:complete:*:*' fzf-preview 'fzf-tab-previewer ${(Q)realpath}'
 zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' fzf-preview 'echo ${(P)word}'
 
 # Previews for some git commands
-zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff $word | delta'|
+zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff $word | delta'
 zstyle ':fzf-tab:complete:git-log:*' fzf-preview 'git log --oneline --pretty=format:"%h %>(12,trunc)%ad %<(12)%aN%d %s" $word'
 zstyle ':fzf-tab:complete:git-help:*' fzf-preview 'git help $word | bat -plman --color=always'
 zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
@@ -60,3 +63,15 @@ zstyle ':fzf-tab:complete:(\\|*/|)man:*' fzf-preview 'man $word | bat -plman --c
 # Preview pacman package info
 zstyle ':fzf-tab:complete:(\\|*/|)pacman:*' fzf-preview 'pacman -Qi $word'
 zstyle ':fzf-tab:complete:(\\|*/|)paru:*' fzf-preview 'paru -Qi $word'
+
+# Preview Homebrew package info
+zstyle ':fzf-tab:complete:brew-(install|uninstall|search|info):*-argument-rest' fzf-preview 'brew info $word'
+
+# Preview SSH config for hosts
+zstyle ':fzf-tab:complete:ssh:*' fzf-preview 'grep -A 10 "^Host $word" ~/.ssh/config 2>/dev/null'
+
+# Preview cargo package info
+zstyle ':fzf-tab:complete:cargo:*' fzf-preview 'cargo search --limit 1 $word 2>/dev/null'
+
+# Preview tmux session info
+zstyle ':fzf-tab:complete:tmux-(attach|switch-client):*' fzf-preview 'tmux list-windows -t $word'
